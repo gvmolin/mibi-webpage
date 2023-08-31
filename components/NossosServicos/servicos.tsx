1ae8306b21b3796ca6952fb1d599167129f33969
Gabriel Molin <molinvinicius@gmail.com>
2023-08-31 19:48:15 -0300

    feat: section two pronta

--- components/NossosServicos/servicos.tsx
+++ components/NossosServicos/servicos.tsx
@@ -2,12 +2,36 @@ import styles from "./style.module.css";
 import GradientLineComponent from "../Details/gradientline";
 
 export default function ServicosComponent (){
+    const steps:{
+        title:string,
+        text:string
+    }[] = [
+        {
+            title:"Branding",
+            text:"Identidade visual, logotipo, cores, tipografia e entre outros elementos visuais."
+        },
+        {
+            title:"Social Media",
+            text:"Processo de criar, planejar, executar e monitorar estratégias e engajamento."
+        },
+        {
+            title:"Mídia Paga",
+            text:"Gerar leads, execução de campanhas com objetivo de aumentar as vendas."
+        },
+        {
+            title:"Sistemas",
+            text:"Criação de sistemas e interfaces para melhorar sua presença online."
+        },
+        {
+            title:"E-mail marketing",
+            text:"Envio de mensagens comerciais ou informativas para uma lista de contatos."
+        },
+    ]
+
     return (
         <div className={styles.container}>
             <section className={styles.sectionOne}>
                 <div>
-                    
-
                 </div>
                 <div style={{width:"40%"}}>
                     <GradientLineComponent />
@@ -17,9 +41,21 @@ export default function ServicosComponent (){
 
             </section>
             <section className={styles.sectionTwo}>
+            <hr />
                 <div className={styles.stepsContainer}>
-                    <hr />
-
+                    <div className={styles.stepsAbsolute}>
+                        {
+                            steps.map(step => (
+                                <div className={styles.step}>
+                                    <div className={styles.stepCircle} />
+                                    <div className={styles.stepContent}>
+                                        <h3>{step.title}</h3>
+                                        <p>{step.text}</p>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
                 </div>
             </section>
         </div>
